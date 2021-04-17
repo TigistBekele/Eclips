3241b856bfdad60ef6402a3f1053f7a1ad6073f6
TigistBekele <ttbtigist2020@gmail.com>
2021-04-16 22:33:35 -0500

    new change

---
+++ firstProject/src/com/firstProject/otherfile.java
@@ -0,0 +1,10 @@
+package com.firstProject;
+
+public class otherfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello world");
+	}
+
+}
